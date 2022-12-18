a4532ab22a3d4763cd9eac01287c3d9aa3c2f551
Dijish <duk@adobe.com>
2022-12-18 14:22:51 +0530

    Day 1

---
+++ Node Server/app.js
@@ -0,0 +1,22 @@
+const http = require('http');
+const fs = require('fs');
+
+const server = http.createServer((req, res) => {
+    const url = req.url;
+    if (url === '/') {
+        res.write('<html>');
+        res.write('<head><title>Enter Message</title><head>');
+        res.write('<body><form action="/message" method="POST"><input type="text" name="message" /><button type="submit">Send</button></form></body>');
+        res.write('</html>');
+        return res.end();
+    }
+    if (url === '/message' && req.method === 'POST') {
+        fs.writeFileSync('message.txt', 'DUMMY');
+        res.statusCode = 302;
+        res.setHeader('Location', '/');
+        return res.end();
+    } 
+    return null;
+});
+
+server.listen(3000);
